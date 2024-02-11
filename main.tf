32e7e70d8403901e845935a19d304b8d3d416a51
Vamsikrishna Mandalapu <vmandalapu@salesforce.com>
2024-02-11 10:39:38 +0530

    creating ec2 instance

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance_1" {
+  ami = "ami-0e731c8a588258d0d"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-demo"
+  }
+}
